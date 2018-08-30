661d890353d4badbc8c1e94bc3e4d3ac7899ec46
prasad <417.prasad@gmail.com>
2018-08-30 16:23:13 -0500

    Adding validation errors

--- src/app/user-registration/containers/registration/registration.component.ts
+++ src/app/user-registration/containers/registration/registration.component.ts
@@ -18,21 +18,122 @@ export class RegistrationComponent implements OnInit {
 
   constructor(private userRegistration: UserRegistrationService, private router: Router) {}
 
+  validation_messages = {
+    'firstName' : [
+      { type: 'required', message: 'First name is required' },
+      { type: 'minlength', message: 'First name must be at least 1 characters long' },
+      { type: 'maxlength', message: 'First name cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'First name must contain only letters' }
+    ],
+    'middleName' : [
+      { type: 'minlength', message: 'Middle name must be at least 1 characters long' },
+      { type: 'maxlength', message: 'Middle name cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'Middle name must contain only letters' }
+    ],
+    'lastName' : [
+      { type: 'required', message: 'Last name is required' },
+      { type: 'minlength', message: 'Last name must be at least 1 characters long' },
+      { type: 'maxlength', message: 'Last name cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'Last name must contain only letters' }
+    ],
+    'phoneNumber' : [
+      { type: 'required', message: 'Phone number is required' },
+      { type: 'minlength', message: 'Phone number must be 10 digits' },
+      { type: 'maxlength', message: 'Phone number must be 10 digits' },
+      { type: 'pattern', message: 'Phone number must contain only numbers' }
+    ],
+    'addressLine1' : [
+      { type: 'required', message: 'Address line1 is required' },
+      { type: 'minlength', message: 'Address line1 must be at least 1 characters long' },
+      { type: 'maxlength', message: 'Address line1 cannot be more than 25 characters long' }
+    ],
+    'addressLine2' : [
+      { type: 'minlength', message: 'Address line2 must be at least 1 characters long' },
+      { type: 'maxlength', message: 'Address line2 cannot be more than 25 characters long' }
+    ],
+    'city' : [
+      { type: 'required', message: 'City is required' },
+      { type: 'minlength', message: 'City must be at least 1 characters long' },
+      { type: 'maxlength', message: 'City cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'City must contain only letters' }
+    ],
+    'state' : [
+      { type: 'required', message: 'State is required' },
+      { type: 'minlength', message: 'State must be at least 1 characters long' },
+      { type: 'maxlength', message: 'State cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'State must contain only letters' }
+    ],
+    'zipcode' : [
+      { type: 'required', message: 'Zipcode is required' },
+      { type: 'minlength', message: 'Zipcode must be 5 digits' },
+      { type: 'maxlength', message: 'Zipcode must be 5 digits' },
+      { type: 'pattern', message: 'Zipcode must contain only numbers' }
+    ],
+    'email' : [
+      { type: 'required', message: 'Email is required' },
+      { type: 'email', message: 'Email must be valid' }
+    ],
+    'password' : [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'Password must be at least 8 characters long' },
+      { type: 'maxlength', message: 'Password cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'Password must contain 1 lowercase, uppercase, number & special character' }
+    ]
+  };
+
   ngOnInit() {
     this.user = new FormGroup({
-      firstName: new FormControl('', [Validators.required, Validators.max(25), Validators.pattern('[a-zA-Z][a-zA-Z ]+')]),
-      middleName: new FormControl(''),
-      lastName: new FormControl('', [Validators.required, Validators.max(25), Validators.pattern('[a-zA-Z][a-zA-Z ]+')]),
-      phoneNumber: new FormControl('', [Validators.required, Validators.minLength(10),
-        Validators.maxLength(10), Validators.pattern('[0-9]*')]),
+      firstName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+        Validators.pattern('[a-zA-Z]+')
+      ])),
+      middleName: new FormControl('', Validators.compose([
+        Validators.minLength(1),
+        Validators.maxLength(25),
+        Validators.pattern('[a-zA-Z]+')
+      ])),
+      lastName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+        Validators.pattern('[a-zA-Z]+')
+      ])),
+      phoneNumber: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        Validators.pattern('[0-9]+')
+      ])),
       // phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{3}[-\s]{0,1}[0-9]{3}[-\s]{0,1}[0-9]{4}$')]),
-      addressLine1: new FormControl('', [Validators.required, Validators.minLength(1), Validators.max(25)]),
-      addressLine2: new FormControl('', [Validators.minLength(0), Validators.maxLength(25)]),
-      city: new FormControl('', [Validators.required, Validators.minLength(1), Validators.max(25),
-        Validators.pattern('[a-zA-Z][a-zA-Z ]+')]),
-      state: new FormControl('', [Validators.required]),
-      zipcode: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(5),
-        Validators.pattern('^[0-9]{5}')]),
+      addressLine1: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+      ])),
+      addressLine2: new FormControl('', Validators.compose([
+        Validators.minLength(1),
+        Validators.maxLength(25),
+      ])),
+      city: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+        Validators.pattern('[a-zA-Z]+')
+      ])),
+      state: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+        Validators.pattern('[a-zA-Z]+')
+      ])),
+      zipcode: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(5),
+        Validators.pattern('^[0-9]{5}')
+      ])),
       // address: new FormGroup({
       //   addressLine1: new FormControl('', [Validators.required]),
       //   addressLine2: new FormControl(''),
@@ -40,10 +141,18 @@ export class RegistrationComponent implements OnInit {
       //   state: new FormControl('', [Validators.required]),
       //   zip: new FormControl('', [Validators.required])
       // }),
-      // tslint:disable-next-line:max-line-length
-      email: new FormControl('', [Validators.required, Validators.pattern('^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20),
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})')])
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+        // tslint:disable-next-line:max-line-length
+        // Validators.pattern('^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(25),
+        Validators.pattern('/(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*-_])[\w!@#$%^&*-_]{8,}$/')
+      ]))
     });
   }
 
